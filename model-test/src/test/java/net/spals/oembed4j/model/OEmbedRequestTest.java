092a47766a96434517ee6aedab3833b13907daef
spags <james.spagnola@salesforce.com>
2016-11-20 17:10:21 -0800

    add setResourceURI as string

--- model-test/src/test/java/net/spals/oembed4j/model/OEmbedRequestTest.java
+++ model-test/src/test/java/net/spals/oembed4j/model/OEmbedRequestTest.java
@@ -17,33 +17,61 @@ public class OEmbedRequestTest {
 
     @DataProvider
     Object[][] toURIProvider() {
-        return new Object[][] {
-                {"https://vimeo.com/api/oembed.{format}",
-                        URI.create("https://vimeo.com/api/oembed.json?url=https%3A%2F%2Fwww.example.com%2F")},
-                {"http://www.youtube.com/oembed",
-                        URI.create("http://www.youtube.com/oembed?format=json&url=https%3A%2F%2Fwww.example.com%2F")},
+        return new Object[][]{
+            {
+                "https://vimeo.com/api/oembed.{format}",
+                URI.create("https://vimeo.com/api/oembed.json?url=https%3A%2F%2Fwww.example.com%2F")
+            },
+            {
+                "http://www.youtube.com/oembed",
+                URI.create("http://www.youtube.com/oembed?format=json&url=https%3A%2F%2Fwww.example.com%2F")
+            },
         };
     }
 
     @Test(dataProvider = "toURIProvider")
-    public void testToURI(final String uriTemplate, final URI expectedURI) {
-        final OEmbedEndpoint endpoint = new OEmbedEndpoint.Builder().addSchemeTemplates("https://www.example.com/*")
-                .setURITemplate(uriTemplate).build();
+    public void testToURIWithURI(final String uriTemplate, final URI expectedURI) {
+        final OEmbedEndpoint endpoint = new OEmbedEndpoint.Builder()
+            .addSchemeTemplates("https://www.example.com/*")
+            .setURITemplate(uriTemplate)
+            .build();
         final OEmbedRequest request = new OEmbedRequest.Builder()
-                .setResourceURI(URI.create("https://www.example.com/")).build();
+            .setResourceURI(URI.create("https://www.example.com/"))
+            .build();
+
+        assertThat(request.toURI(endpoint), is(expectedURI));
+    }
+
+    @Test(dataProvider = "toURIProvider")
+    public void testToURIWithString(final String uriTemplate, final URI expectedURI) {
+        final OEmbedEndpoint endpoint = new OEmbedEndpoint.Builder()
+            .addSchemeTemplates("https://www.example.com/*")
+            .setURITemplate(uriTemplate)
+            .build();
+        final OEmbedRequest request = new OEmbedRequest.Builder()
+            .setResourceURI("https://www.example.com/")
+            .build();
 
         assertThat(request.toURI(endpoint), is(expectedURI));
     }
 
     @Test
     public void testToURIWithMaxHeightAndMaxWidth() {
-        final OEmbedEndpoint endpoint = new OEmbedEndpoint.Builder().addSchemeTemplates("https://www.example.com/*")
-                .setURITemplate("http://www.youtube.com/oembed").build();
+        final OEmbedEndpoint endpoint = new OEmbedEndpoint.Builder()
+            .addSchemeTemplates("https://www.example.com/*")
+            .setURITemplate("http://www.youtube.com/oembed")
+            .build();
         final OEmbedRequest request = new OEmbedRequest.Builder()
-                .setResourceURI(URI.create("https://www.example.com/"))
-                .setMaxHeight(1).setMaxWidth(2).build();
+            .setResourceURI(URI.create("https://www.example.com/"))
+            .setMaxHeight(1)
+            .setMaxWidth(2)
+            .build();
 
-        assertThat(request.toURI(endpoint),
-                is(URI.create("http://www.youtube.com/oembed?format=json&maxheight=1&maxwidth=2&url=https%3A%2F%2Fwww.example.com%2F")));
+        assertThat(
+            request.toURI(endpoint),
+            is(
+                URI.create(
+                    "http://www.youtube.com/oembed?format=json&maxheight=1&maxwidth=2&url=https%3A%2F%2Fwww.example.com%2F"))
+        );
     }
 }
